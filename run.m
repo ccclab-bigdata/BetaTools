e27e571cc833e6fb70eea66c797e20cfeba1d48a
Matt <matt@primestudiosllc.com>
2014-09-23 08:44:36 -0400

    final commit from weekend

--- run.m
+++ run.m
@@ -1,5 +1,6 @@
-% subplot(4,1,1);
-% plotSpikeRaster(allSpikes');
-% xlim([0 xend]);
-% title('All Spikes Raster');
-% ylabel('channel');
+p=primes(1000000);
+nums=[pi .3232 rand(1,3)*5];
+for i=1:length(nums)
+    figure;
+    hist(mod(p,nums(i)),300);
+end
