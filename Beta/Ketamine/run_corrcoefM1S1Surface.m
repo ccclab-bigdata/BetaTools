4ab052caf6926be969373c26c1063c7a580f8ba8
Matt Gaidica <matt@gaidi.ca>
2014-11-23 18:21:45 -0500

    created difference matrix over all files

--- Beta/Ketamine/run_corrcoefM1S1Surface.m
+++ Beta/Ketamine/run_corrcoefM1S1Surface.m
@@ -1,29 +1,31 @@
-trials = 25; %30s
-sampleStart = zNew(1,5).CerebusTimeStart; %see notes file
-sampleStop = sampleStart+((trials*5000)/1000)*3e4; %5000 is the timeout
-sampleLength = sampleStop-sampleStart;
+% NSx = openNSx('read');
+plot(NSx.Data(1,:));
+disp('Select start/stop points of clean data...');
+[x,~]=ginput;
+x = round(x);
+disp(['data length:',num2str(x(2)-x(1))]);
 
-% dataM1=NS4.Data(1:16,sampleStart:end);
-% dataS1=NS4.Data(65:80,sampleStart:end);
+dataM1=NSx.Data(1:16,x(1):x(2));
+dataS1=NSx.Data(end-15:end,x(1):x(2));
 
-dataM1=NS5.Data(1:16,sampleStart:sampleStart+3e6);
-dataS1=NS5.Data(17:32,sampleStart:sampleStart+3e6);
-
-usefilter = '40-70Hz_butter_30kHz.mat';
+usefilter = '13-30Hz_butter_30kHz.mat';
 [phasesS1,filtDataS1] = extractBandpassPhase(dataS1,usefilter);
 [phasesM1,filtDataM1] = extractBandpassPhase(dataM1,usefilter);
 
-data = zeros(16,16);
+data = zeros(16);
 for s1i=1:16
     for m1i=1:16
         [r,p] = corrcoef(phasesS1(s1i,:),phasesM1(m1i,:));
         data(s1i,m1i) = r(1,2);
     end
 end
+
 figure;
 imagesc(data);
 colorbar;
 caxis([min(data(:)) max(data(:))]);
 xlabel('M1');
 ylabel('S1');
-title('run002 gamma');
+title(strcat('20140916-144027-011.ns5,x1=',num2str(x(1)),',x2=',num2str(x(2))));
+
+data_20140916_144027_011_ns5 = data;
---
+++ Beta/Ketamine/run_similarityMatrix.m
@@ -0,0 +1,27 @@
+allData = [];
+allData(1,:,:) = data_20140916_140408_002_ns4;
+allData(2,:,:) = data_20140916_144027_001_ns5;
+allData(3,:,:) = data_20140916_144027_002_ns5;
+allData(4,:,:) = data_20140916_144027_003_ns5;
+allData(5,:,:) = data_20140916_144027_004_ns5;
+allData(6,:,:) = data_20140916_144027_005_ns5;
+allData(7,:,:) = data_20140916_144027_006_ns5;
+allData(8,:,:) = data_20140916_144027_007_ns5;
+allData(9,:,:) = data_20140916_144027_008_ns5;
+allData(10,:,:) = data_20140916_144027_009_ns5;
+allData(11,:,:) = data_20140916_144027_010_ns5;
+allData(12,:,:) = data_20140916_144027_011_ns5;
+
+ii=size(allData,1);
+diffMatrix = zeros(ii);
+for i=1:ii
+    data1=squeeze(allData(i,:,:));
+    for j=1:ii
+        data2=squeeze(allData(j,:,:));
+        diffMatrix(i,j) = mean(mean(std(data1-data2)));
+    end
+end
+
+figure;
+imagesc(diffMatrix);
+colorbar;
