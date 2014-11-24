beeb4efe3c05f5f8bcd44424d78028400f2f8e25
Matt Gaidica <matt@gaidi.ca>
2014-11-24 15:13:24 -0500

    building matrix with good data chucks (pieces) for before and after files

--- Beta/Ketamine/ketamineMatrix.m
+++ Beta/Ketamine/ketamineMatrix.m
@@ -1,6 +1,6 @@
 function allMatrices=ketamineMatrix(beforeDir,afterDir)
     beforeFiles = dir(fullfile(beforeDir,'*.ns4'));
-    afterFiles = dir(fullfile(afterDir,'*.ns4'));
+    afterFiles = dir(fullfile(afterDir,'*.ns5'));
     
     allMatrices = [];
     pieceCount = 1;
@@ -12,12 +12,41 @@ function allMatrices=ketamineMatrix(beforeDir,afterDir)
         % could average data first, assumes ch1 is representative
         pieces = findCleanSpans(NSx.Data(1,:),thresh,chopSamples);
         disp(['pieces:',num2str(length(pieces))]);
+        hbar = waitbar(0,beforeFiles(i).name);
         for j=1:length(pieces)
-            hbar = waitbar(j/length(pieces),beforeFiles(i).name);
+            hbar = waitbar(j/length(pieces),hbar);
             dataM1 = NSx.Data(1:16,pieces(j,1):pieces(j,2));
             dataS1 = NSx.Data(end-15:end,pieces(j,1):pieces(j,2));
             
             usefilter = '13-30Hz_butter_10kHz.mat';
+            [phasesS1,filtDataS1] = extractBandpassPhase(dataS1,usefilter);
+            [phasesM1,filtDataM1] = extractBandpassPhase(dataM1,usefilter);
+            
+            allMatrices(pieceCount,:,:) = zeros(16);
+            for s1i=1:16
+                for m1i=1:16
+                    [r,p] = corrcoef(phasesS1(s1i,:),phasesM1(m1i,:));
+                    allMatrices(pieceCount,s1i,m1i) = r(1,2);
+                end
+            end
+            pieceCount = pieceCount+1;
+        end
+    end
+    
+    chopSamples = 5*3e4; %5s @ 30kS/s
+    for i=1:1%length(beforeFiles)
+        disp(afterFiles(i).name);
+        NSx = openNSx(fullfile(afterDir,afterFiles(i).name),'read');
+        % could average data first, assumes ch1 is representative
+        pieces = findCleanSpans(NSx.Data(1,:),thresh,chopSamples);
+        disp(['pieces:',num2str(length(pieces))]);
+        hbar = waitbar(0,afterFiles(i).name);
+        for j=1:length(pieces)
+            hbar = waitbar(j/length(pieces),hbar);
+            dataM1 = NSx.Data(1:16,pieces(j,1):pieces(j,2));
+            dataS1 = NSx.Data(end-15:end,pieces(j,1):pieces(j,2));
+            
+            usefilter = '13-30Hz_butter_30kHz.mat';
             [phasesS1,filtDataS1] = extractBandpassPhase(dataS1,usefilter);
             [phasesM1,filtDataM1] = extractBandpassPhase(dataM1,usefilter);
             
